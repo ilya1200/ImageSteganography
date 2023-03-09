ed0303b51c93749a320a06e7d1415c26cb55e65e
ilya <ilya1200@gmail.com>
2023-03-09 18:52:45 +0200

    refactor app.py

--- app.py
+++ app.py
@@ -6,11 +6,10 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 load_dotenv()
 
-SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
-SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
-
 
 def main():
+    SLACK_APP_TOKEN: str = os.environ["SLACK_APP_TOKEN"]
+    SLACK_BOT_TOKEN: str = os.environ["SLACK_BOT_TOKEN"]
     app = App(token=SLACK_BOT_TOKEN, name="ImageSteganography")
     handler = SocketModeHandler(app, SLACK_APP_TOKEN)
     handler.start()
