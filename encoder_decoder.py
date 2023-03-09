4108170a273b61fd96d2c36e1b707a3909b7d2e9
ilya <ilya1200@gmail.com>
2023-03-09 09:29:29 +0200

    add message_to_binary

---
+++ encoder_decoder.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+import types
+
+
+class EncoderDecoder:
+
+    @staticmethod
+    def _message_to_binary(message):
+        if type(message) == str:
+            return ''.join([format(ord(i), "08b") for i in message])
+        elif type(message) == bytes or type(message) == np.ndarray:
+            return ''.join([format(i, "08b") for i in message])
+        elif type(message) == int or type(message) == np.uint8:
+            return format(message, "08b")
+        else:
+            raise TypeError("Input type not supported")
+
+    @staticmethod
+    def encode(image, secret_message: str):
+        pass
+
+    @staticmethod
+    def decode(image) -> str:
+        pass
+
+
+print(EncoderDecoder._message_to_binary("hello"))
