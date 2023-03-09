f5406d0f202ef24429e54aa38f56c52cedab7a8c
ilya <ilya1200@gmail.com>
2023-03-09 09:44:04 +0200

    refactor message_to_binary

--- encoder_decoder.py
+++ encoder_decoder.py
@@ -1,20 +1,28 @@
+from logging import Logger
 import cv2
 import numpy as np
 import types
+import my_logger
+
+logger: Logger = my_logger.get_console_logger(__name__)
 
 
 class EncoderDecoder:
 
     @staticmethod
-    def _message_to_binary(message):
+    def _message_to_binary(message) -> str:
+        logger.debug(f"Message: {message}")
+        binary_message = None
         if type(message) == str:
-            return ''.join([format(ord(i), "08b") for i in message])
+            binary_message = ''.join([format(ord(i), "08b") for i in message])
         elif type(message) == bytes or type(message) == np.ndarray:
-            return ''.join([format(i, "08b") for i in message])
+            binary_message = ''.join([format(i, "08b") for i in message])
         elif type(message) == int or type(message) == np.uint8:
-            return format(message, "08b")
+            binary_message = format(message, "08b")
         else:
             raise TypeError("Input type not supported")
+        logger.debug(f"type: {type(message)},binary_message: {binary_message}")
+        return binary_message
 
     @staticmethod
     def encode(image, secret_message: str):
