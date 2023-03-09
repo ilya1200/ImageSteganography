1655d37a71c5935741c3abb9bce85eaed7f11a3e
ilya <ilya1200@gmail.com>
2023-03-09 14:01:28 +0200

    encode image

--- encoder_decoder.py
+++ encoder_decoder.py
@@ -1,7 +1,9 @@
 from logging import Logger
+
 import cv2
+import numpy
 import numpy as np
-import types
+
 import my_logger
 
 logger: Logger = my_logger.get_console_logger(__name__)
@@ -12,7 +14,7 @@ class EncoderDecoder:
     @staticmethod
     def _message_to_binary(message) -> str:
         logger.debug(f"Message: {message}")
-        binary_message = None
+        binary_message: str = str()
         if type(message) == str:
             binary_message = ''.join([format(ord(i), "08b") for i in message])
         elif type(message) == bytes or type(message) == np.ndarray:
@@ -25,9 +27,44 @@ class EncoderDecoder:
         return binary_message
 
     @staticmethod
-    def encode(image, secret_message: str):
-        pass
+    def encode(image: numpy.ndarray, secret_message: str) -> numpy.ndarray:
+        logger.info(f"Encoding {secret_message} into:\n{image}")
+        DELIMITER: str = "#####"
+
+        # calculate the maximum bytes to encode
+        image_bytes: int = image.shape[0] * image.shape[1] * 3
+        logger.debug(f"Maximum bytes to encode:{image_bytes}")
+
+        # Check if the number of bytes to encode is less than the maximum bytes in the image
+        if len(secret_message) > image_bytes:
+            logger.error(
+                f"Error encountered insufficient bytes. Secret message is {len(secret_message)} bytes, but the image is {image_bytes} bytes.")
+            raise ValueError("Error encountered insufficient bytes, need bigger image or less data !!")
+
+        secret_message += DELIMITER
+
+        # Convert the cover image to a 1D numpy array
+        image_1d: numpy.ndarray = image.flatten()
+
+        secret_message_bin: str = EncoderDecoder._message_to_binary(secret_message)
+        data_to_hide_len: int = len(secret_message_bin)
+
+        # Modify the least significant bit of each pixel to store the secret message
+        for pixel_index in range(data_to_hide_len):
+            image_1d[pixel_index] = (image_1d[pixel_index] & 254) | int(secret_message_bin[pixel_index])
+
+        # Reshape the modified 1D array to the original shape of the cover image
+        stego_img: numpy.ndarray = np.reshape(image_1d, image.shape)
+
+        logger.info(f"The encoded image:\n{stego_img}")
+        return stego_img
 
     @staticmethod
-    def decode(image) -> str:
+    def decode(image: numpy.ndarray) -> str:
         pass
+
+
+secret_message: str = "hi"
+image_array: list = cv2.imread("images/balloons.png")
+image: numpy.ndarray = numpy.array(image_array)
+EncoderDecoder.encode(image, secret_message)
