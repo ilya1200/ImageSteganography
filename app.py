3120cc688c2c847d58c26835697d7122ecd700cb
ilya <ilya1200@gmail.com>
2023-03-09 20:36:14 +0200

    update app and config

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 import os
 
 from dotenv import load_dotenv
+from omegaconf import OmegaConf
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+from base_directory import base_directory
+
 load_dotenv()
+SLACK_APP_TOKEN: str = os.environ["SLACK_APP_TOKEN"]
+SLACK_BOT_TOKEN: str = os.environ["SLACK_BOT_TOKEN"]
+
+app: App = App(token=SLACK_BOT_TOKEN, name="Joke Bot")
+cfg = OmegaConf.load(f"{base_directory}/config/config.yaml")
+
+# Set the channel ID where you want to listen for file uploads
+channel_id = cfg.slack_bot.channel_id
 
 
 def main():
-    APP_NAME: str = "ImageSteganography"
-    app = App(token=os.environ["SLACK_BOT_TOKEN"], name=APP_NAME)
-    handler = SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"])
+    handler = SocketModeHandler(app, SLACK_APP_TOKEN)
     handler.start()
 
 
