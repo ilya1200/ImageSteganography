6945cd2b84f49d46fea75c938e052fa3a1ccaf5a
ilya <ilya1200@gmail.com>
2023-03-11 11:51:09 +0200

    update test_decode

--- tests/test_encoder_decoder.py
+++ tests/test_encoder_decoder.py
@@ -18,21 +18,14 @@ def test_message_to_binary(secret_message):
         assert int(char) in (0, 1)
 
 
-@pytest.mark.parametrize("image_path, secret_message", [
-    (f"{base_directory}/images/balloons.png", "Hello"),
-    (f"{base_directory}/images/squirrel.png", "World")
+@pytest.mark.parametrize("encoded_image_path, expected_secret_message", [
+    (f"{base_directory}/images_for_testing/encoded/Hello_balloons.png", "Hello"),
+    (f"{base_directory}/images_for_testing/encoded/World_squirrel.png", "World")
 ])
-def test_decode(image_path: str, secret_message: str):
-    image: numpy.ndarray = cv2.imread(image_path)
-    stego_img: numpy.ndarray = EncoderDecoder.encode(image, secret_message)
-    assert stego_img.size == image.size
-
-    stego_img_path: str = f"{base_directory}/images/stego_{os.path.basename(image_path)}"
-    cv2.imwrite(stego_img_path, stego_img)
-    assert os.path.exists(stego_img_path) and os.path.isfile(stego_img_path)
-
-    os.remove(stego_img_path)
-    assert True
+def test_decode(encoded_image_path: str, expected_secret_message: str):
+    encoded_image: numpy.ndarray = cv2.imread(encoded_image_path)
+    actual_secret_message: str = EncoderDecoder.decode(encoded_image)
+    assert actual_secret_message == expected_secret_message
 
 
 @pytest.mark.parametrize("image_path, secret_message", [
